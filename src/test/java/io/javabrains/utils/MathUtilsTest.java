55ba4b45ca0984fd05dafe3c98ffd92debf01d88
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-04-18 11:27:04 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

--- src/test/java/io/javabrains/utils/MathUtilsTest.java
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -7,10 +7,9 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
-	
+
 	private MathUtils mathUtils;
-	
-	
+
 	@BeforeEach
 	void initEach() {
 		mathUtils = new MathUtils();
@@ -18,45 +17,39 @@ class MathUtilsTest {
 
 	@Nested
 	class AddTest {
+
 		@Test
 		void testAddingTwoPositives() {
-			assertEquals(2, mathUtils.add(1, 1), 
-					"Add method should return the sum of two numbers");
+			assertEquals(2, mathUtils.add(1, 1), "Add method should return the sum of two numbers");
 		}
-		
+
 		@Test
 		void testAddingTwoNegatives() {
-			assertEquals(-2, mathUtils.add(-1, -1), 
-					"Add method should return the sum of two numbers");
+			assertEquals(-2, mathUtils.add(-1, -1), "Add method should return the sum of two numbers");
 		}
-		
+
 		@Test
 		void testAddingAPositiveAndANegative() {
-			assertEquals(0, mathUtils.add(-1, 1), 
-					"Add method should return the sum of two numbers");
+			assertEquals(0, mathUtils.add(-1, 1), "Add method should return the sum of two numbers");
 		}
+
 	}
-	
-	@Test 
+
+	@Test
 	void testMultiply() {
-		assertAll(
-				() -> assertEquals(0, mathUtils.multiply(1, 0)),
-				() -> assertEquals(1, mathUtils.multiply(1, 1)),
-				() -> assertEquals(6, mathUtils.multiply(2, 3))
-				);
+		assertAll(() -> assertEquals(0, mathUtils.multiply(1, 0)), () -> assertEquals(1, mathUtils.multiply(1, 1)),
+				() -> assertEquals(6, mathUtils.multiply(2, 3)));
 	}
-	
-	@Test 
+
+	@Test
 	void computeCircleArea() {
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), 
-				"Should return right circle area");
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
-	
+
 	@Test
 	void testDivide() {
-		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
 				"Divide should throw ArithmeticException when denominator is zero");
 	}
-	
-	
+
 }
